0fb130767a5a78bb06874a671a68bf5b87a10afe
ShawnReardon <shawnreardon1@gmail.com>
2021-04-26 15:32:58 -0400

    Initial files for MakeCode project

---
+++ images.g.ts
@@ -0,0 +1,37 @@
+// Auto-generated code. Do not edit.
+namespace myImages {
+
+    helpers._registerFactory("image", function(name: string) {
+        switch(helpers.stringTrim(name)) {
+            case "/+^(wt-#":
+            case "sddrdsdfdg":return img`
+. . . . . . . . . . . . . . . . 
+. . . . . . 7 7 e . . . . . . . 
+. . . . e 7 7 e e e 7 . . . . . 
+. . . . . . d d d . . . . . . . 
+. . . . . . 8 d 8 . . . . . . . 
+. . . . . . d d d . . . . . . . 
+. . . . . d . d . d . . . . . . 
+. . . . d e 7 e e e d . . . . . 
+. . . d . 7 7 d 7 e . d . . . . 
+. . . . . 7 d e d 7 . . . . . . 
+. . . d . e e d 7 7 . d . . . . 
+. . . . d e e . e e d . . . . . 
+. . . . . d . . e d . . . . . . 
+. . . . e e . . e e . . . . . . 
+. . e e e e . . e e e e . . . . 
+. . e e e e . . e e e e . . . . 
+`;
+        }
+        return null;
+    })
+
+    helpers._registerFactory("animation", function(name: string) {
+        switch(helpers.stringTrim(name)) {
+
+        }
+        return null;
+    })
+
+}
+// Auto-generated code. Do not edit.
